8ae519c260976eccd8b28a979c166a7dd8a96db3
Maninderjit Bindra <mani@Saurabhs-MacBook-Pro.local>
2017-08-21 18:17:17 +0530

    Added Dockerfile and .dockerignore

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+var eventName = process.env.EVENT_NAME || 'Express';
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: eventName });
 });
 
 module.exports = router;
